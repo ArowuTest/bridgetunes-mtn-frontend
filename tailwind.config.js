b9f145edb89a270c63c6fda946b1d053d0d10628
Favour George C <phavorsparks@gmail.com>
2025-05-02 14:46:42 +0100

    configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        interstate: ["Interstate", "sans-serif"],
+        winner: ["WinnerSans", "sans-serif"],
+      },
+
+      animation: {
+        "progress-loader": "progressLoader 1.5s ease-in-out infinite",
+      },
+      keyframes: {
+        progressLoader: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
